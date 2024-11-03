13ffa75e1dc5d26923f5f17742138602c1b06791
LepharamRamchiary <lepharamchiary@gmail.com>
2024-11-03 11:04:51 +0530

    simple modification

--- application/views/adminpanel/dashboard.php
+++ application/views/adminpanel/dashboard.php
@@ -6,7 +6,7 @@ $this->load->view('adminpanel/header');
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Dashboard</h1>
     </div>
-    <div class="row d-flex justify-content-center text-center">
+    <div class="d-flex-columns justify-content-center text-center">
         <div class="alert alert-primary" role="alert">
             <a href="<?= base_url() . 'admin/blog' ?>">View Blogs</a>
         </div>
--- application/views/adminpanel/loginview.php
+++ application/views/adminpanel/loginview.php
@@ -42,12 +42,12 @@
 
         ?>
 
-        <img class="mb-4" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+        <!-- <img class="mb-4" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72"> -->
         <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
         <label for="inputEmail" class="sr-only">Username</label>
-        <input type="text" name="email" id="inputEmail" class="form-control" placeholder="Username" required="" autofocus="">
+        <input type="text" name="email" id="inputEmail" class="form-control"  placeholder="Username" required="" autofocus="">
         <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+        <input type="password" name="password" id="inputPassword" class="form-control" style="margin-top: 10px;" placeholder="Password" required="">
         <div class="checkbox mb-3">
             <label>
                 <input type="checkbox" value="remember-me"> Remember me
