fe771bc1ef01eb651ca6cf85bd230cc2968d6777
LepharamRamchiary <lepharamchiary@gmail.com>
2024-11-05 12:04:03 +0530

    add blog details page

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,23 +1,31 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Home extends CI_Controller
+{
 
-	public function index()
-	{
+    public function index()
+    {
         $this->load->model('BlogModel', "bm");
-        $result=$this->bm->fetch_all_blog();
+        $result = $this->bm->fetch_all_blog();
         // echo "<pre>";
         // print_r($result);
         // die();
 
         $data['result'] = $result;
 
-		$this->load->view('blog_list_page', $data);
-	}
+        $this->load->view('blog_list_page', $data);
+    }
 
-    function blog_detail($blog_id=0)
+    function blog_detail($blog_id = 0)
     {
-        
+        // print_r($blog_id);
+        // die();
+
+        $this->load->model('BlogModel', "bm");
+        $result = $this->bm->fetch_blog_detail($blog_id);
+        $data['result'] = $result;
+
+        $this->load->view('blog_detail', $data);
     }
 }
--- application/models/BlogModel.php
+++ application/models/BlogModel.php
@@ -7,4 +7,11 @@ class BlogModel extends CI_Model
 
         return $result->result_array();
     }
+
+    function fetch_blog_detail($blog_id)
+    {
+        $result = $this->db->query(" SELECT `blogid`, `blog_title`, `blog_desc`, `blog_img`, `status`, `created_on`, `updated` FROM `articles` WHERE blogid = '$blog_id' ");
+
+        return $result->result_array();
+    }
 }
